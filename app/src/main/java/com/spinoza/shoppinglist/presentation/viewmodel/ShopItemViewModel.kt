e772b669566b68a979a05b1754d92eaca920bfac
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-01-11 15:00:27 +0300

    add methods to ShopItemViewModel

--- app/src/main/java/com/spinoza/shoppinglist/presentation/viewmodel/ShopItemViewModel.kt
+++ app/src/main/java/com/spinoza/shoppinglist/presentation/viewmodel/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.spinoza.shoppinglist.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.spinoza.shoppinglist.domain.ShopItem
 import com.spinoza.shoppinglist.domain.ShopListRepository
@@ -12,7 +14,25 @@ class ShopItemViewModel(repository: ShopListRepository) : ViewModel() {
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    fun getShopItem(shopItemId: Int): ShopItem = getShopItemUseCase.getShopItem(shopItemId)
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    private val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    fun getShopItem(shopItemId: Int) {
+        _shopItem.value = getShopItemUseCase.getShopItem(shopItemId)
+    }
 
     fun addShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
@@ -20,15 +40,19 @@ class ShopItemViewModel(repository: ShopListRepository) : ViewModel() {
         if (validateInput(name, count)) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
     }
 
-    fun editShopItem(shopItem: ShopItem, inputName: String?, inputCount: String?) {
+    fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         if (validateInput(name, count)) {
-            val newShopItem = ShopItem(name, count, shopItem.enabled, shopItem.id)
-            editShopItemUseCase.editShopItem(newShopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
@@ -43,14 +67,27 @@ class ShopItemViewModel(repository: ShopListRepository) : ViewModel() {
     private fun validateInput(name: String, count: Float): Boolean {
         var result = true
         if (name.isBlank()) {
-            // TODO: show error input name
+            _errorInputName.value = true
             result = false
         }
         if (count <= 0) {
-            // TODO: show error input count
+            _errorInputCount.value = true
             result = false
         }
 
         return result
     }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
+    }
+
 }
